690cc1bdf55da67b7cc668640e0d5c368b9815a8
Tito2007 <107062136+Tito2007@users.noreply.github.com>
2026-02-12 14:09:52 -0500

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de Supabase
-const SUPABASE_URL = 'https://wyornkwbqgboopjoevtu.supabase.co';
+const SUPABASE_URL = 'wyornkwbqgboopjoevtu';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5b3Jua3dicWdib29wam9ldnR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MzI3NDUsImV4cCI6MjA4MDIwODc0NX0.xFg6gJbPsO9b-iHOn2dhxXnyTSJIDhVveoJRu2xpWI8';
 
 // Inicializar Supabase
