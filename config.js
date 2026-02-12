be800c007d8971265ef28ef46867497a54b23d34
Tito2007 <107062136+Tito2007@users.noreply.github.com>
2026-02-12 14:45:17 -0500

    Rename supabase variable to supabaseclient

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = 'https://wyornkwbqgboopjoevtu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5b3Jua3dicWdib29wam9ldnR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDYzMjc0NSwiZXhwIjoyMDgwMjA4NzQ1fQ.lpQdtdr01aNgyYm__aIvyjanOKRSHplFthleu2wz36M';
 
 // Inicializar Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseclient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
